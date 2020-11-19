5d102f947e990d2788c7e7802ea1b7219ef441fb
gleek908i <am_bsk_love_rubio@yahoo.co.jp>
2020-11-19 22:50:50 +0900

    create class components and functional component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-function App() {
-  return <div><h1>Hello,World!</h1></div>;
+// function App() {
+//   return <div><h1>Hello,World!</h1></div>;
   
+// }
+
+
+const App = () => {
+  return (
+    <div>
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+    </div>
+  )
 }
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
 
 export default App;
